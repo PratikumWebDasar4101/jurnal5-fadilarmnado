3cc710136a25d4ae83e98c2d03239fdd2ad5b347
fadilarmnado <42725259+fadilarmnado@users.noreply.github.com>
2018-10-01 10:23:13 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
-</body>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Registrasi</title>
+  </head>
+  <body>
+    <form action="progresregis.php" method="post">
+      <center>
+      <h1>Pendaftaran</h1>
+      NIM : <input type="text" name="nim" maxlength="10" pattern="\d*" >
+      Nama : <input type="text" name="nama" maxlength="25" required>
+      Email : <input type="email" name="email" pattern="[a-z0-9._-]+@+[a-z]+.com"><br><br>
+      <input type="submit" name="submit" value="Registrasi">
+    </center>
+    </form>
+  </body>
 </html>
