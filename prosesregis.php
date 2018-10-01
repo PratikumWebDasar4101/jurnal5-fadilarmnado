4eaa209905867d2bf0111705905d591e382a94cb
fadilarmnado <42725259+fadilarmnado@users.noreply.github.com>
2018-10-01 10:22:43 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,16 @@
 <?php
-
-
-
-
-
-?>
+  $nim = $_POST['nim'];
+  $nama = $_POST['nama'];
+  $email = $_POST['email'];
+  echo "<h2>Input</h2>";
+  echo "NIM :"." ";
+  echo $nim;
+  echo "<br>";
+  echo "Nama :"." ";
+  echo $nama;
+  echo "<br>";
+  echo "Email :"." ";
+  echo $email;
+  echo "<br>";
+  ?>
+  <a href="registrasi.php">Logout</a>
